7b75ce3c258ecd67af91ce6fe94292799b6ce864
Dawson <dpeng1@hwemail.com>
2026-02-16 10:50:19 -0800

    basic platformer working

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public float flapStrength;
+    public float moveSpeed = 5f;
+    private Rigidbody2D body;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    public bool IsGrounded;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(movement * moveSpeed, body.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, flapStrength);
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        IsGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+}
