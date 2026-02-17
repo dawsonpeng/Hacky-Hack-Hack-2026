9cfa5ad735df036538b1b1ab19326892e9b54387
JamesEG <jgraczyk1@hwemail.com>
2026-02-16 22:35:31 -0800

    Created a Powerup which allows you to change the day
    maybe brightness slider later?

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class SettingsScript : MonoBehaviour
 {
+    public Volume volume;
+    private ColorAdjustments colorAdjust;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        volume.profile.TryGet(out colorAdjust);
     }
 
     // Update is called once per frame
@@ -23,5 +28,20 @@ public class SettingsScript : MonoBehaviour
 
     public void ChangeVolume(float deltaVol) {
         AudioListener.volume += deltaVol;
+    }
+
+    [ContextMenu("debright")]
+    public void ToggleDay() {
+        if (colorAdjust.postExposure.value < 0) {
+            ChangeBrightness(0f);
+        } else {
+            ChangeBrightness(-4f);
+        }
+    }
+
+    // changes the brightness somehow??? GPT ahh
+    public void ChangeBrightness(float value)
+    {
+        colorAdjust.postExposure.value = value;
     }
 }
