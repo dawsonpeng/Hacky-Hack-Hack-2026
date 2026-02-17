fdaedbd04d1b1892a8e681580f84b8b22e8f5281
Matthew <mren1@hwemail.com>
2026-02-17 14:19:49 -0800

    Some main menu and levelselector sound also level selector update

--- Assets/Scripts/levelselectorScript.cs
+++ Assets/Scripts/levelselectorScript.cs
@@ -5,32 +5,37 @@ public class levelselectorScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void level1(){
+    public void level1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void level2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void level3()
+    {
+        //todo
+    }
+
+    public void level4()
+    {
+        //todo
+    }
+    public void level5()
+    {
         SceneManager.LoadScene("MainScene");
-    }
-
-    public void level2(){
-        //todo
-    }
-
-    public void level3(){
-        //todo
-    }
-
-    public void level4(){
-        //todo
-    }
-    public void level5(){
-        //todo
     }
 
     public void BackToMainMenu()
